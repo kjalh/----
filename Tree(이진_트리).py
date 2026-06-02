df0eb79297f38aa9638e4052818e16a92d5c3074
희승ACMY <heseung0895@naver.com>
2026-06-02 18:04:10 +0900

    트리 기본 구조

---
+++ Tree(이진_트리).py
@@ -0,0 +1,59 @@
+class Tree:
+    def __init__(self):
+        pass
+
+    def in_data(self):
+
+        return ""
+        pass
+    
+
+    def out_data(self):
+
+        return ""
+        pass
+
+
+
+    def find_data(self):
+
+        find_menu = input("pre/in/post : ")
+
+
+        while True:
+            match find_menu:
+                case "pre":
+
+                    break
+                    pass
+                
+                case "in":
+                    break
+                    pass
+
+                case "post":
+                    break
+                    pass
+
+                case _:
+                    print("다시 입력")
+
+        return ""
+
+
+
+
+while True:
+    menu = input("값 입력: ")
+
+    if menu == "in":
+        pass
+    elif menu == "out":
+        pass
+    elif menu == "find":
+        pass
+    elif menu == "0":
+        exit()
+    else:
+        print("다시\n")
+
